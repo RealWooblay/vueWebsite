0e135545b904b922ba7a8077dfb31fe74ddbe549
RealWooblay <jackaustinc66@gmail.com>
2023-02-22 12:15:35 +1100

    Button Orig

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,27 +1,52 @@
 <template>
-    <button class="storybook-button" @click="handleClick">
-      <slot />
-    </button>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Button',
-    methods: {
-      handleClick() {
-        this.$emit('click');
+  <button type="button" :class="classes" @click="onClick" :style="style">{{ label }}</button>
+</template>
+
+<script>
+import './button.css';
+import { reactive, computed } from 'vue';
+
+export default {
+  name: 'my-button',
+
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    primary: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String,
+      validator: function (value) {
+        return ['small', 'medium', 'large'].indexOf(value) !== -1;
+      },
+    },
+    backgroundColor: {
+      type: String,
+    },
+  },
+
+  emits: ['click'],
+
+  setup(props, { emit }) {
+    props = reactive(props);
+    return {
+      classes: computed(() => ({
+        'storybook-button': true,
+        'storybook-button--primary': props.primary,
+        'storybook-button--secondary': !props.primary,
+        [`storybook-button--${props.size || 'medium'}`]: true,
+      })),
+      style: computed(() => ({
+        backgroundColor: props.backgroundColor,
+      })),
+      onClick() {
+        emit('click');
       }
     }
-  };
-  </script>
-  
-  <style scoped>
-  .storybook-button {
-    font-size: 1em;
-    padding: 0.25em 1em;
-    border: 2px solid #0074D9;
-    border-radius: 3px;
-    color: #0074D9;
-    background-color: white;
-  }
-  </style>
+  },
+};
+</script>
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,30 +1,52 @@
-import { action } from '@storybook/addon-actions';
+import MyButton from '../components/Button.vue';
 
-import Button from '../components/Button.vue';
-
+// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
 export default {
-  title: 'Button',
-  component: Button,
+  title: 'Controls/Button',
+  component: MyButton,
+  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
   argTypes: {
-    onClick: { action: 'clicked' },
+    backgroundColor: { control: 'color' },
+    onClick: {},
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'large'],
+    },
   },
 };
 
-const Template = (args, { argTypes }) => ({
-  components: { Button },
-  props: Object.keys(argTypes),
-  methods: { action: action('clicked') },
-  template: '<Button @click="action"><slot /></Button>',
+// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
+const Template = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { MyButton },
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    return { args };
+  },
+  // And then the `args` are bound to your component with `v-bind="args"`
+  template: '<my-button v-bind="args" />',
 });
 
 export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/vue/writing-stories/args
 Primary.args = {
-  class: 'storybook-button--primary',
-  children: 'Primary Button',
+  primary: true,
+  label: 'Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  class: 'storybook-button--secondary',
-  children: 'Secondary Button',
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
 };
