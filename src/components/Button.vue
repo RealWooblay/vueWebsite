324d6593469bb21812ca26b814dacf2d6f31ddf1
RealWooblay <jackaustinc66@gmail.com>
2023-02-22 11:28:50 +1100

    Button

---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+    <button class="storybook-button" @click="handleClick">
+      <slot />
+    </button>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Button',
+    methods: {
+      handleClick() {
+        this.$emit('click');
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .storybook-button {
+    font-size: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid #0074D9;
+    border-radius: 3px;
+    color: #0074D9;
+    background-color: white;
+  }
+  </style>
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,52 +1,30 @@
-import MyButton from './Button.vue';
+import { action } from '@storybook/addon-actions';
 
-// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
+import Button from '../components/Button.vue';
+
 export default {
-  title: 'Controls/Button',
-  component: MyButton,
-  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
+  title: 'Button',
+  component: Button,
   argTypes: {
-    backgroundColor: { control: 'color' },
-    onClick: {},
-    size: {
-      control: { type: 'select' },
-      options: ['small', 'medium', 'large'],
-    },
+    onClick: { action: 'clicked' },
   },
 };
 
-// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
-const Template = (args) => ({
-  // Components used in your story `template` are defined in the `components` object
-  components: { MyButton },
-  // The story's `args` need to be mapped into the template through the `setup()` method
-  setup() {
-    return { args };
-  },
-  // And then the `args` are bound to your component with `v-bind="args"`
-  template: '<my-button v-bind="args" />',
+const Template = (args, { argTypes }) => ({
+  components: { Button },
+  props: Object.keys(argTypes),
+  methods: { action: action('clicked') },
+  template: '<Button @click="action"><slot /></Button>',
 });
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/vue/writing-stories/args
 Primary.args = {
-  primary: true,
-  label: 'Button',
+  class: 'storybook-button--primary',
+  children: 'Primary Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+  class: 'storybook-button--secondary',
+  children: 'Secondary Button',
 };
